2c8c139e7b564f2ad9e1903a74f036da9d0563b0
Jason Lengstorf <jason@lengstorf.com>
2020-05-21 10:42:52 -0700

    site ready for deployment

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+it('loads the home page and navigates to about', () => {
+  cy.visit('public/index.html');
+
+  cy.contains('h1', 'This Page Is Tested');
+
+  cy.get('[data-test]').click();
+
+  cy.contains('h1', 'About');
+
+  cy.get('.load-character').click();
+
+  cy.get('.loaded-data').within(() => {
+    cy.get('h2').should('exist').and('not.contain', 'Want to see');
+    cy.get('.character-image').should('exist');
+  });
+});
